e0f07a48aaa5ea8ccae83700f68742af695ffabe
Harsh Dalmia <harshdalmia20212081@gmail.com>
2024-10-16 15:38:50 +0530

    Loader spinner

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import ExpandableSection from './ExpandableSection';
-import godownData from '../Data/godowns.json'
-import ItemData from '../Data/items.json'
+import { Circles } from 'react-loader-spinner'; // Importing the loader component
 
 const buildGodownTreeWithItems = (godowns, items) => {
     const godownMap = {};  
 
-  
     godowns.forEach(godown => {
         godownMap[godown.id] = { ...godown, subLocations: [], items: [] };
     });
@@ -19,7 +17,6 @@ const buildGodownTreeWithItems = (godowns, items) => {
         }
     });
 
-  
     godowns.forEach(godown => {
         if (godown.parent_godown) {
             godownMap[godown.parent_godown].subLocations.push(godownMap[godown.id]);
@@ -37,6 +34,8 @@ const Sidebar = ({ onSelectItem }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchLocations, setSearchLocations] = useState('');
     const [filterCategory, setFilterCategory] = useState('All');
+    const [loading, setLoading] = useState(true); 
+    const [error, seterror] = useState('')
 
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value);
@@ -49,66 +48,84 @@ const Sidebar = ({ onSelectItem }) => {
         setFilterCategory(e.target.value);
     };
 
-
     useEffect(() => {
-        fetch('https://warehouse-management-backend-qxu0.onrender.com/locations')
-            .then(res => res.json())
-            .then(data => setLocations(data));
        
-        fetch('https://warehouse-management-backend-qxu0.onrender.com/items')
-            .then(res => res.json())
-            .then(data => setItems(data));
+        const fetchData = async () => {
+            try {
+                const locationsResponse = await fetch('https://warehouse-management-backend-qxu0.onrender.com/locations');
+                const itemsResponse = await fetch('https://warehouse-management-backend-qxu0.onrender.com/items');
+                const locationsData = await locationsResponse.json();
+                const itemsData = await itemsResponse.json();
 
-       
+                setLocations(locationsData);
+                setItems(itemsData);
+            } catch (error) {
+                setLoading(false);
+                seterror('Error fetching data');
+            } finally {
+                setLoading(false); 
+            }
+        };
+
+        fetchData();
     }, []);
 
     const godownTree = buildGodownTreeWithItems(locations, items);
-   console.log(godownTree)
+
     return (
         <div className="sidebar bg-[#644536] md:w-full min-w-screen max-h-screen overflow-y-scroll">
-            <div className="search-bar">
-                <input
-                    type="text"
-                    placeholder="Search for items"
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                    className="search-input p-2 border border-gray-300 rounded-md bg-transparent w-full mb-5"
-                />
-            </div>
-            <div className="search-bar">
-                <input
-                    type="text"
-                    placeholder="Search for locations"
-                    value={searchLocations}
-                    onChange={handleSearch2Change}
-                    className="search-input p-2 border border-gray-300 rounded-md bg-transparent w-full mb-5"
-                />
-            </div>
+            {loading ? ( 
+                <div className="flex justify-center items-center h-full space-x-2 ">
+                    <Circles color="#DA8E42" height={20} width={20} />
+                   <span className='text-[#DA8E42] '> Loading</span>
+                </div>
+            ) : (
+                <>
+                    <div className="search-bar">
+                        <input
+                            type="text"
+                            placeholder="Search for items"
+                            value={searchQuery}
+                            onChange={handleSearchChange}
+                            className="search-input p-2 border border-gray-300 rounded-md bg-transparent w-full mb-5"
+                        />
+                    </div>
+                    <div className="search-bar">
+                        <input
+                            type="text"
+                            placeholder="Search for locations"
+                            value={searchLocations}
+                            onChange={handleSearch2Change}
+                            className="search-input p-2 border border-gray-300 rounded-md bg-transparent w-full mb-5"
+                        />
+                    </div>
 
-            <div className="filter-bar mb-5">
-                <select
-                    value={filterCategory}
-                    onChange={handleCategoryChange}
-                    className="filter-select p-2 border border-gray-300  w-full rounded-md bg-transparent"
-                >
-                    <option value="All">All Categories</option>
-                    <option value="Toys">Toys</option>
-                    <option value="Electronics">Electronics</option>
-                    <option value="Furniture">Furniture</option>
-                    <option value="Clothing">Clothing</option>
-                    {/* Add other categories as needed */}
-                </select>
-            </div>
-            {godownTree?.map(godown => (
-                <ExpandableSection
-                    key={godown.id}
-                    godown={godown}
-                    onSelectItem={onSelectItem}
-                    searchQuery={searchQuery}
-                    searchLocations={searchLocations}
-                    filterCategory={filterCategory}
-                />
-            ))}
+                    <div className="filter-bar mb-5">
+                        <select
+                            value={filterCategory}
+                            onChange={handleCategoryChange}
+                            className="filter-select p-2 border border-gray-300  w-full rounded-md bg-transparent"
+                        >
+                            <option value="All">All Categories</option>
+                            <option value="Toys">Toys</option>
+                            <option value="Electronics">Electronics</option>
+                            <option value="Furniture">Furniture</option>
+                            <option value="Clothing">Clothing</option>
+                        </select>
+                    </div>
+                    {godownTree?.map(godown => (
+                        <ExpandableSection
+                            key={godown.id}
+                            godown={godown}
+                            onSelectItem={onSelectItem}
+                            searchQuery={searchQuery}
+                            searchLocations={searchLocations}
+                            filterCategory={filterCategory}
+                        />
+                    ))}
+                    <p className='text-center text-white'>{error}</p>
+                </>
+            )}
         </div>
     );
 };
